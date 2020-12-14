8362799596486976cc105af8f7478eb05848707d
VishalTalatii <x20131020@student.ncirl.ie>
2020-12-14 19:50:08 +0530

    testing

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Employee Management System app is running!');
+    expect(page.getTitleText()).toEqual('Employee_Management_System app is running!');
   });
 
   afterEach(async () => {
